88e769aff7852baf143f697ad43e816a01e39436
Ira <nikulochkina.irina@yandex.ru>
2022-04-21 14:42:31 +0300

    adapted to mobile devices, added a js dropdown

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
-span.onclick = function() {
+};
+span.onclick = function () {
   modal.style.display = "none";
-}
-window.onclick = function(event) {
+};
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+};
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn-header")) {
+    var dropdowns = document.getElementsByClassName("dropdown-header");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
